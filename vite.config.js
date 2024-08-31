aa68876e39b327be34902d3e2f07295114f66938
bernarddwumfour <bernarddwumfour25@gmail.com>
2024-08-31 22:27:37 +0000

    Added notification page

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: ['./index.html', './sheet.html', './views/docs.html'],
+      input: ['./index.html', './sheet.html', './views/docs.html','./notification.html'],
     },
   },
 });
